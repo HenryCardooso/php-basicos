489ce7be83f8a3b45842d2eeea3e8e8f953f23d7
Henry Cardoso <henry.cardoso@aluno.senai.br>
2025-10-17 08:54:16 -0300

    Desafio
    Desafio concluido

---
+++ 1_helloworld.php
@@ -0,0 +1,6 @@
+<?php
+
+//exibe mensagem
+echo "Hello World"
+
+?>
---
+++ Aula1/exemplo1.php
@@ -0,0 +1,14 @@
+<?php
+    //G= Geração de texto (string)
+    echo "Chama Sesi-Senai!";
+
+    /*
+    Geração de texto estruturado
+    Com tags e atributos
+    */
+    // Aspas simples dentro das aspas duplas
+    echo "<h2>Gerando <span class='destaque'>HTML</span> atraves do PHP.</h2>";
+
+    //Aspas dupla dentro de aspas duplas
+    echo "<h2>Gerando <span class=\"destaque\">HTML</span> atraves do PHP.</h2>";
+?>
